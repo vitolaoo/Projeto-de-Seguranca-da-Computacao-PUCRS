8ffa96279a124ce2cd8f0c9b2c6d99032f5714a6
Arthur Vitola <142800673+vitolaoo@users.noreply.github.com>
2024-09-08 18:05:54 -0300

    Update Tarefa21.py

--- Tarefa21.py
+++ Tarefa21.py
@@ -1,10 +1,13 @@
 import random
 
+seed = 12345  
+random.seed(seed)
+
 # Valores fornecidos
 p = 1041607122029938459843911326429539139964006065005940226363139
 g = 10
 
-# Gerar um valor b aleatório com pelo menos 40 dígitos e menor que p
+# Gerar um valor b com pelo menos 40 dígitos e menor que p, usando a seed
 b = random.randint(10**39, p - 1)
 
 # Calcular B = g^b mod p
@@ -13,3 +16,12 @@ B = pow(g, b, p)
 # Mostrar o valor de b e B
 print(f"Valor de b (decimal): {b}")
 print(f"Valor de B (decimal): {B}")
+
+# Valor fornecido por Alice
+A = 105008283869277434967871522668292359874644989537271965222162
+
+# Calcular v = A^b mod p
+v = pow(A, b, p)
+
+# Mostrar o valor de v
+print(f"Valor de v (decimal): {v}")
